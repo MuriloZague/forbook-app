008211e4e2428fe17c12a66013f594cdac5ba77f
MuriloZague <zaguemurilo7@gmail.com>
2026-03-31 19:34:40 -0300

    styles: ajusting bg color

--- app/announce.tsx
+++ app/announce.tsx
@@ -50,7 +50,7 @@ export default function Modal() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#ffffff",
   },
   title: {
     fontSize: 24,
